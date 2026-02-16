0cd3df0d6515e132fb446ffb60924b71c3fd708b
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-02-16 17:17:26 -0300

    Criação de algumas rotas, criação do header, Logo, Login e entre outros

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css'
+import Header from './Components/Header'
+import Fotter from './Components/Footer'
+import Login from './Components/Login/Login'
+import Home from './Components/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const App = () => {
+  return <div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/login' element={<Login/>} />
+    </Routes>
+    <Fotter/>
+    </BrowserRouter>
+  </div>;
+};
+
+export default App;
