7956830a4bcb29ebac3d14892e35a62872c640fd
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-02-18 16:52:10 -0300

    UserContext, Header, Atualizações

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import Fotter from './Components/Footer'
 import Login from './Components/Login/Login'
 import Home from './Components/Home'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { UserStorage } from './UserContext';
 
 const App = () => {
   return <div>
     <BrowserRouter>
+    <UserStorage>
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>} />
       <Route path='/login/*' element={<Login/>} />
     </Routes>
     <Fotter/>
+    </UserStorage>
     </BrowserRouter>
   </div>;
 };
